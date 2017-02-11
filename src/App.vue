683d292ac9bbe3dff858f774a53805feb3cb6f08
Kairos <kairos@computer.local>
2017-02-11 21:52:03 +0000

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app" >
     <mainnav></mainnav>
     <router-view></router-view>
+    <bottom></bottom>
     </div>
   </div>
 </template>
 <script>
 import welcome from './components/views/welcome-view';
 import mainnav from './components/page_assets/mainnav';
+import bottom from './components/page_assets/bottom';
 
 export default {
   name: 'app',
   components: {
     welcome,
     mainnav,
+    bottom,
   },
 };
 </script>
---
+++ src/components/page_assets/bottom.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <footer class="footer">
+            <p class="text-muted">
+                Place text here
+            </p>
+        </footer>
+    </div>
+</template>
+<style lang="sass?indentedSyntax">
+
+.footer 
+  position: absolute
+  right: 0
+  bottom: 0
+  left: 0
+  padding: 5px 0px 0px 20px
+  background-color: #efefef
+  text-align: left
+    
+</style>
